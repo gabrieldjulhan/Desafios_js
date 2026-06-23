55fbe6955a5613229c7828d7f77ebeab97697375
gabrieldjulhan <gabrieldjulhan@gmail.com>
2026-06-22 21:45:53 -0300

    desafio loja concluido

--- desafio04/loja.js
+++ desafio04/loja.js
@@ -1,3 +1,10 @@
 const prond=[
-    
+    {nome:'Notbook', valor:2400},
+    {nome:'Ram', Valor:300},
+    {nome:'SD', valor:80}
 ]
+for(let c of prod){
+    if (c.Valor>100){
+        console.log(`Um produto acima de 100 reais é ${c.nome}, custando ${c.Valor}.`)
+    }
+}
