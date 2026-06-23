5fa309edb2f522eba2b0a696f54f6050b6c99453
gabrieldjulhan <gabrieldjulhan@gmail.com>
2026-06-22 21:47:29 -0300

    concluido

---
+++ desafio06/filme.js
@@ -0,0 +1,12 @@
+
+const filme = {
+  titulo: "Interestelar",
+  genero: "Ficção Científica",
+  ano: 2014
+};
+function exibirFilme(filme) {
+  console.log(`Título: ${filme.titulo}
+Gênero: ${filme.genero}
+Ano: ${filme.ano}`);
+}
+exibirFilme(filme);
